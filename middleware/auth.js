f86c9850a4bf3c2986d415be8c7f42232e049dda
Trung Pham <trungpham2012@gmail.com>
2019-07-25 11:28:38 -0500

    fix auth issue

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,6 +1,8 @@
 const config = require('config');
 const jwt = require('jsonwebtoken');
 
+const jwtSecret = process.env.jwtSecret || config.get('jwtSecret');
+
 const auth = (req, res, next) => {
   const token = req.header('x-auth-token');
 
@@ -9,7 +11,7 @@ const auth = (req, res, next) => {
 
   //Verify token
   try {
-    const decoded = jwt.verify(token, config.get('jwtSecret'));
+    const decoded = jwt.verify(token, config.get(jwtSecret));
     // Add user  from payload
     req.user = decoded;
     next();
